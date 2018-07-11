6e95e0480841d0efa7bc19ea7dfeeab2aef2060c
Kaughlin Caver <kaughlin@fb.com>
2018-07-10 18:04:07 -0700

    day commit

--- app/src/main/java/codepath/kaughlinpractice/kaughlin_insta/my_fragments/createPics.java
+++ app/src/main/java/codepath/kaughlinpractice/kaughlin_insta/my_fragments/createPics.java
@@ -7,20 +7,29 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.io.File;
 
 import codepath.kaughlinpractice.kaughlin_insta.R;
+import codepath.kaughlinpractice.kaughlin_insta.model.Post;
 
 import static android.app.Activity.RESULT_OK;
 
 
 public class createPics extends Fragment {
+    //private static final String imagePath = "/storage/emulated/0/DCIM/Camera/IMG_20180709_175930.jpg";
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -30,6 +39,8 @@ public class createPics extends Fragment {
     File photoFile;
     Button btnCamera;
     ImageView userPic;
+    EditText description;
+    Button post;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,15 +56,26 @@ public class createPics extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         btnCamera = view.findViewById(R.id.btnCamButton);
+
+
+        userPic = view.findViewById(R.id.ivUserPic);
+        userPic.setVisibility(View.INVISIBLE);
+        description = view.findViewById(R.id.etDescription);
+        description.setVisibility(View.INVISIBLE);
+        post = view.findViewById(R.id.btnPost);
+        post.setVisibility(View.INVISIBLE);
+
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dispatchTakePictureIntent(); // Intent
+                //final ParseFile image = (ParseFile) userPic.getParseFile("");
             }
         });
 
 
-        userPic = view.findViewById(R.id.ivUserPic);
+
+
 
     }
 
@@ -64,6 +86,25 @@ public class createPics extends Fragment {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             userPic.setImageBitmap(imageBitmap); // image that what just took post in on imageview i created on fragment
+            userPic.setVisibility(View.VISIBLE);
+            btnCamera.setVisibility(View.INVISIBLE);
+            description.setVisibility(View.VISIBLE);
+            post.setVisibility(View.VISIBLE);
+
+/*
+
+            post.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final String descriptionInput = description.getText().toString();
+                    final ParseUser user = ParseUser.getCurrentUser();
+                    final File file = new File(imagePath);
+                    final ParseFile parseFile = new ParseFile(file);
+
+                    createPost(descriptionInput, parseFile, user);
+                }
+            });
+*/
         }
     }
 
@@ -72,6 +113,27 @@ public class createPics extends Fragment {
         if (takePictureIntent.resolveActivity(createPics.this.getActivity().getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
+    }
+
+    private void createPost(String description, ParseFile imageFile, ParseUser user){
+        final Post newPost = new Post();
+        newPost.setDescription(description);
+        newPost.setImage(imageFile);
+        newPost.setUser(user);
+
+        newPost.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e == null){
+                    //Toast.makeText(bottomNavActivity.this,"Post successful", Toast.LENGTH_LONG ).show();
+
+                    Log.d("home", "create post success!");
+                }
+                else{
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 }
--- app/src/main/java/codepath/kaughlinpractice/kaughlin_insta/my_fragments/home.java
+++ app/src/main/java/codepath/kaughlinpractice/kaughlin_insta/my_fragments/home.java
@@ -2,11 +2,18 @@ package codepath.kaughlinpractice.kaughlin_insta.my_fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+
+import java.util.List;
+
 import codepath.kaughlinpractice.kaughlin_insta.R;
+import codepath.kaughlinpractice.kaughlin_insta.model.Post;
 
 
 public class home extends Fragment {
@@ -16,6 +23,30 @@ public class home extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    private void loadTopPosts() {
+
+        final Post.Query postsQuery = new Post.Query();
+        postsQuery.getTop().withUser();
+
+
+        postsQuery.findInBackground(new FindCallback<Post>()
+
+        {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); i++) {
+                        Log.d("HomeActivity", "post[" + i + "]= " + objects.get(i).getDescription()
+                                + "\nusername =" + objects.get(i).getUser().getUsername());
+                    }
+
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 
